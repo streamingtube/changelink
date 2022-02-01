fe8199b1b3972cfb98a99b7c449a37ad41a8079b
streamingtube <82635690+streamingtube@users.noreply.github.com>
2022-01-31 23:26:11 -0800

    Create changelinkmain.js

---
+++ changelinkmain.js
@@ -0,0 +1,67 @@
+ $(document).ready(function(){
+        $('a[href^="https://"]').each(function(){ 
+            var oldUrl = $(this).attr("href"); // Get current url
+            var newUrl = oldUrl.replace("https://yourearningadvisor.blogspot.com/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=https://yourearningadvisor.blogspot.com/"); // Create new url
+            $(this).attr("href", newUrl); // Set herf value
+        });
+    });
+  
+  $(document).ready(function(){
+        $('a[href^="https://"]').each(function(){ 
+            var oldUrl = $(this).attr("href"); // Get current url
+            var newUrl = oldUrl.replace("https://13x4.com/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=https://13x4.com/"); // Create new url
+            $(this).attr("href", newUrl); // Set herf value
+        });
+    });
+  
+  $(document).ready(function(){
+        $('a[href^="https://"]').each(function(){ 
+            var oldUrl = $(this).attr("href"); // Get current url
+            var newUrl = oldUrl.replace("http://videos.udemyonlinecourses.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=http://videos.udemyonlinecourses.workers.dev/"); // Create new url
+            $(this).attr("href", newUrl); // Set herf value
+        });
+    });
+  
+  $(document).ready(function(){
+        $('a[href^="http://"]').each(function(){ 
+            var oldUrl = $(this).attr("href"); // Get current url
+            var newUrl = oldUrl.replace("https://courses.computerbasics.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=https://courses.computerbasics.workers.dev/"); // Create new url
+            $(this).attr("href", newUrl); // Set herf value
+        });
+    });
+
+
+$(document).ready(function(){
+        $('a[href^="http://"]').each(function(){ 
+            var oldUrl = $(this).attr("href"); // Get current url
+            var newUrl = oldUrl.replace("http://best.typingtricks.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=http://best.typingtricks.workers.dev/"); // Create new url
+            $(this).attr("href", newUrl); // Set herf value
+        });
+    });
+
+
+$(document).ready(function(){
+        $('a[href^="http://"]').each(function(){ 
+            var oldUrl = $(this).attr("href"); // Get current url
+            var newUrl = oldUrl.replace("http://top.studymaterials.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=http://top.studymaterials.workers.dev/"); // Create new url
+            $(this).attr("href", newUrl); // Set herf value
+        });
+    });
+
+
+$(document).ready(function(){
+        $('a[href^="http://"]').each(function(){ 
+            var oldUrl = $(this).attr("href"); // Get current url
+            var newUrl = oldUrl.replace("http://things.thingsforitdiploma.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=http://things.thingsforitdiploma.workers.dev/"); // Create new url
+            $(this).attr("href", newUrl); // Set herf value
+        });
+    });
+
+
+$(document).ready(function(){
+        $('a[href^="https://"]').each(function(){ 
+            var oldUrl = $(this).attr("href"); // Get current url
+            var newUrl = oldUrl.replace("https://documents.examnotespdf.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=https://documents.examnotespdf.workers.dev/"); // Create new url
+            $(this).attr("href", newUrl); // Set herf value
+        });
+    });
