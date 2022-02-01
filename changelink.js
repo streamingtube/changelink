1ccaea02fa03a78c9bd8ca94b823d2a0b54529df
streamingtube <82635690+streamingtube@users.noreply.github.com>
2022-01-31 23:23:15 -0800

    Update changelink.js

--- changelink.js
+++ changelink.js
@@ -1,7 +1,7 @@
  $(document).ready(function(){
         $('a[href^="https://"]').each(function(){ 
             var oldUrl = $(this).attr("href"); // Get current url
-            var newUrl = oldUrl.replace("https://yourearningadvisor.blogspot.com/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&amp;url=https://yourearningadvisor.blogspot.com/"); // Create new url
+            var newUrl = oldUrl.replace("https://yourearningadvisor.blogspot.com/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=https://yourearningadvisor.blogspot.com/"); // Create new url
             $(this).attr("href", newUrl); // Set herf value
         });
     });
@@ -9,7 +9,7 @@
   $(document).ready(function(){
         $('a[href^="https://"]').each(function(){ 
             var oldUrl = $(this).attr("href"); // Get current url
-            var newUrl = oldUrl.replace("https://13x4.com/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&amp;url=https://13x4.com/"); // Create new url
+            var newUrl = oldUrl.replace("https://13x4.com/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=https://13x4.com/"); // Create new url
             $(this).attr("href", newUrl); // Set herf value
         });
     });
@@ -17,7 +17,7 @@
   $(document).ready(function(){
         $('a[href^="https://"]').each(function(){ 
             var oldUrl = $(this).attr("href"); // Get current url
-            var newUrl = oldUrl.replace("http://videos.udemyonlinecourses.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&amp;url=http://videos.udemyonlinecourses.workers.dev/"); // Create new url
+            var newUrl = oldUrl.replace("http://videos.udemyonlinecourses.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=http://videos.udemyonlinecourses.workers.dev/"); // Create new url
             $(this).attr("href", newUrl); // Set herf value
         });
     });
@@ -25,7 +25,7 @@
   $(document).ready(function(){
         $('a[href^="http://"]').each(function(){ 
             var oldUrl = $(this).attr("href"); // Get current url
-            var newUrl = oldUrl.replace("https://courses.computerbasics.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&amp;url=https://courses.computerbasics.workers.dev/"); // Create new url
+            var newUrl = oldUrl.replace("https://courses.computerbasics.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=https://courses.computerbasics.workers.dev/"); // Create new url
             $(this).attr("href", newUrl); // Set herf value
         });
     });
@@ -34,7 +34,7 @@
 $(document).ready(function(){
         $('a[href^="http://"]').each(function(){ 
             var oldUrl = $(this).attr("href"); // Get current url
-            var newUrl = oldUrl.replace("http://best.typingtricks.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&amp;url=http://best.typingtricks.workers.dev/"); // Create new url
+            var newUrl = oldUrl.replace("http://best.typingtricks.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=http://best.typingtricks.workers.dev/"); // Create new url
             $(this).attr("href", newUrl); // Set herf value
         });
     });
@@ -43,7 +43,7 @@ $(document).ready(function(){
 $(document).ready(function(){
         $('a[href^="http://"]').each(function(){ 
             var oldUrl = $(this).attr("href"); // Get current url
-            var newUrl = oldUrl.replace("http://top.studymaterials.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&amp;url=http://top.studymaterials.workers.dev/"); // Create new url
+            var newUrl = oldUrl.replace("http://top.studymaterials.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=http://top.studymaterials.workers.dev/"); // Create new url
             $(this).attr("href", newUrl); // Set herf value
         });
     });
@@ -52,7 +52,7 @@ $(document).ready(function(){
 $(document).ready(function(){
         $('a[href^="http://"]').each(function(){ 
             var oldUrl = $(this).attr("href"); // Get current url
-            var newUrl = oldUrl.replace("http://things.thingsforitdiploma.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&amp;url=http://things.thingsforitdiploma.workers.dev/"); // Create new url
+            var newUrl = oldUrl.replace("http://things.thingsforitdiploma.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=http://things.thingsforitdiploma.workers.dev/"); // Create new url
             $(this).attr("href", newUrl); // Set herf value
         });
     });
@@ -61,7 +61,7 @@ $(document).ready(function(){
 $(document).ready(function(){
         $('a[href^="https://"]').each(function(){ 
             var oldUrl = $(this).attr("href"); // Get current url
-            var newUrl = oldUrl.replace("https://documents.examnotespdf.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&amp;url=https://documents.examnotespdf.workers.dev/"); // Create new url
+            var newUrl = oldUrl.replace("https://documents.examnotespdf.workers.dev/", "https://shortgalaxy.in/st?api=b987d5006ed6438610fe06a08be30c3b4bbafbbe&url=https://documents.examnotespdf.workers.dev/"); // Create new url
             $(this).attr("href", newUrl); // Set herf value
         });
     });
